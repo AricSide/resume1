25b5c8e6c75c89cbc9a3dc4dbac17167982cd236
AricSide <1769210790@QQ.com>
2016-07-07 11:44:07 +0800

    简历

---
+++ js/index.js
@@ -0,0 +1,52 @@
+
+FastClick.attach(document.body);
+~function () {
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / 640 * 200 + "px";
+}();
+new Swiper(".swiper-container", {
+    loop: true,
+    direction: "vertical",
+    onSlidePrevEnd: changeEnd,
+    onSlideNextEnd: changeEnd,
+});
+function changeEnd(swiper) {
+    var n = swiper.activeIndex,
+        slideAry = swiper.slides;
+    [].forEach.call(slideAry, function (slide, index) {
+        if (n === index) {
+            if(n==0){
+                slide.id = "page4";
+            } else if(n==5) {
+                slide.id = "page1";
+            }else{
+                slide.id = "page"+n;
+            }
+            return;
+        }
+        slide.id = null;
+    });
+}
+var music = document.getElementById("music"),
+    musicAudio = document.getElementById("musicAudio");
+window.setTimeout(function () {
+    musicAudio.play();
+    musicAudio.addEventListener("canplay", function () {
+        music.style.display = "block";
+        music.className = "music move";
+    }, false);
+}, 1000);
+music.addEventListener("click", function () {
+    if (musicAudio.paused) {
+        musicAudio.play();
+        music.className = "music move";
+        return;
+    }
+    musicAudio.pause();
+    music.className = "music";
+}, false);
+
+
+
+
+
